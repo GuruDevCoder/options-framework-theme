c1124d644fe4d5939884a6a72eb1e8d961ff2a53
Erics-Bai <Eric.Bai0214@hotmail.com>
2023-05-13 06:34:31 +0900

    options-framework-theme
    options-framework-theme

---
+++ admin/js/options-custom.js
@@ -0,0 +1,133 @@
+/**
+ * Prints out the inline javascript needed for the colorpicker and choosing
+ * the tabs in the panel.
+ */
+
+jQuery(document).ready(function($) {
+	
+	// Fade out the save message
+	$('.fade').delay(1000).fadeOut(1000);
+	
+	
+
+	// Slider
+	$('.slider').each(function(index,element){
+		var sl_min,sl_max,sl_step;
+		if($(this).attr('min'))
+		sl_min = $(this).attr('min');
+		else
+		sl_min = 0;
+		if($(this).attr('max'))
+		sl_max = $(this).attr('max');	
+		else
+		sl_max = 100;	
+		if($(this).attr('step'))
+		sl_step = $(this).attr('step');	
+		else
+		sl_step = 1;		
+		$(this).slider({
+        	min: parseInt(sl_min),  
+        	max: parseInt(sl_max),  
+			step:parseInt(sl_step),
+			value:parseInt($(this).next().find('input').val()),
+        	slide: function(e,ui){   
+			$(this).next().find('input').attr('value',ui.value);
+        	}  
+    	});
+	});	
+	
+	
+	
+	
+	
+	// Color Picker
+	$('.colorSelector').each(function(){
+		var Othis = this; //cache a copy of the this variable for use inside nested function
+		var initialColor = $(Othis).next('input').attr('value');
+		$(this).ColorPicker({
+		color: initialColor,
+		onShow: function (colpkr) {
+		$(colpkr).fadeIn(500);
+		return false;
+		},
+		onHide: function (colpkr) {
+		$(colpkr).fadeOut(500);
+		return false;
+		},
+		onChange: function (hsb, hex, rgb) {
+		$(Othis).children('div').css('backgroundColor', '#' + hex);
+		$(Othis).next('input').attr('value','#' + hex);
+	}
+	});
+	}); //end color picker
+	
+	// Switches option sections
+	$('.group').hide();
+	var activetab = '';
+	if (typeof(localStorage) != 'undefined' ) {
+		activetab = localStorage.getItem("activetab");
+	}
+	if (activetab != '' && $(activetab).length ) {
+		$(activetab).fadeIn();
+	} else {
+		$('.group:first').fadeIn();
+	}
+	$('.group .collapsed').each(function(){
+		$(this).find('input:checked').parent().parent().parent().nextAll().each( 
+			function(){
+				if ($(this).hasClass('last')) {
+					$(this).removeClass('hidden');
+						return false;
+					}
+				$(this).filter('.hidden').removeClass('hidden');
+			});
+	});
+	
+	if (activetab != '' && $(activetab + '-tab').length ) {
+		$(activetab + '-tab').addClass('nav-tab-active');
+	}
+	else {
+		$('.nav-tab-wrapper a:first').addClass('nav-tab-active');
+	}
+	$('.nav-tab-wrapper a').click(function(evt) {
+		$('.nav-tab-wrapper a').removeClass('nav-tab-active');
+		$(this).addClass('nav-tab-active').blur();
+		var clicked_group = $(this).attr('href');
+		if (typeof(localStorage) != 'undefined' ) {
+			localStorage.setItem("activetab", $(this).attr('href'));
+		}
+		$('.group').hide();
+		$(clicked_group).fadeIn();
+		evt.preventDefault();
+	});
+           					
+	$('.group .collapsed input:checkbox').click(unhideHidden);
+				
+	function unhideHidden(){
+		if ($(this).attr('checked')) {
+			$(this).parent().parent().parent().nextAll().removeClass('hidden');
+		}
+		else {
+			$(this).parent().parent().parent().nextAll().each( 
+			function(){
+				if ($(this).filter('.last').length) {
+					$(this).addClass('hidden');
+					return false;		
+					}
+				$(this).addClass('hidden');
+			});
+           					
+		}
+	}
+	
+	// Image Options
+	$('.of-radio-img-img').click(function(){
+		$(this).parent().parent().find('.of-radio-img-img').removeClass('of-radio-img-selected');
+		$(this).addClass('of-radio-img-selected');		
+	});
+		
+	$('.of-radio-img-label').hide();
+	$('.of-radio-img-img').show();
+	$('.of-radio-img-radio').hide();
+		 		
+});	
